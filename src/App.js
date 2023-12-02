966f44737f5b814ed52212831f1f5c3f81ac5810
nirbhaykaushik44@gmail.com <Nirbhay91>
2023-12-02 14:21:32 +0530

    hotel booking

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Listofhotels from './Hotels/ListOfHotels';
+
+function App() {
+  return (
+    <div className="App">
+      <Listofhotels />
+    </div>
+  );
+}
+
+export default App;
